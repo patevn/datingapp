6e30bfed854e983274a467c1cd776b5933e73147
pete mc <petermmcnaught@gmail.com>
2024-04-20 11:19:45 +1000

    clean some boilerplate and add entity and lite db programatically

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,7 +1,7 @@
-namespace API;
+﻿namespace API.Entities;
 
 public class AppUser
 {
     public int Id { get; set; }
-    public string? UserName { get; set; }
+    public string UserName { get; set;}
 }
