df20c92d476f0878b649a0e09ff6879b1eca5ccf
pete mc <petermmcnaught@gmail.com>
2024-05-01 09:21:31 +1000

    add ssl to client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -12,10 +12,15 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
-    this.http.get('https:/localhost:5001/api/users').subscribe({
+    this.http.get(environment.apiUrl + '/users').subscribe({
       next: (response) => (this.users = response),
       error: (error) => console.log(error),
       complete: () => console.log('Request is complete'),
     });
   }
 }
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:5001/api',
+};
