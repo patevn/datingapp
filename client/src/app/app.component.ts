b31aa7b41b3c08643c94dda22aa0a55c2de4d9c1
pete mc <petermmcnaught@gmail.com>
2024-05-08 15:41:43 +1000

    shift get users logic into home

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from './_services/account.service';
 import { User } from './_types/User';
@@ -12,18 +11,9 @@ export class AppComponent implements OnInit {
   title = 'Dating App';
   users: any;
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
-  }
-
-  getUsers() {
-    this.http.get(environment.apiUrl + '/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request is complete'),
-    });
   }
 
   setCurrentUser() {
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../app.component';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../_types/User';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
+  users: User | undefined;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getUsers();
+  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
+  }
+
+  getUsers() {
+    this.http.get(environment.apiUrl + '/users').subscribe({
+      next: (response: any) => (this.users = response),
+      error: (error: any) => console.log(error),
+      complete: () => console.log('Request is complete'),
+    });
   }
 }
