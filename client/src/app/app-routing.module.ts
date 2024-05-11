882326f09ab6c704651cf605987317f7631e1c31
pete mc <petermmcnaught@gmail.com>
2024-05-11 11:14:00 +1000

    add the router links to the new components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
-const routes: Routes = [];
+// the empty string strter is what happens with no path on the url,
+// then it works its way down array looking for matches, if no match, it uses the wildcard **
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'members', component: MemberListComponent },
+  { path: 'members/:id', component: MemberDetailComponent },
+  { path: 'lists', component: ListsComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: '**', component: HomeComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
